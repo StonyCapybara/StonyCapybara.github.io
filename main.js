8e18f810e5afe52c8c76dc68d8e6107b76a469da
StonyCapybara <97652916+StonyCapybara@users.noreply.github.com>
2022-01-13 13:40:44 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+function openy() {
+  document.write("")
+  window.open("https://youtube.com", "_self");
+  return "Check";
+}
+
+function openyk() {
+  document.write("")
+  window.open("https://krunker.io", "_self");
+  return "Check";
+}
